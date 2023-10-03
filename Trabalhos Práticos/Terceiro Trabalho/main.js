ea718af27addccccec7f8940835d66d161e9fb2c
dioic3 <joyce.dias@gec.inatel.br>
2023-10-02 21:46:11 -0300

    adição dos comentários da carol no código

--- Trabalhos Práticos/Terceiro Trabalho/main.js
+++ Trabalhos Práticos/Terceiro Trabalho/main.js
@@ -1,44 +1,49 @@
-class Process {
-    constructor(id, arrivalTime, burstTime) {
+class Process { // classe process
+    constructor(id, arrivalTime, burstTime) { // 3 atributos
       this.id = id;
-      this.arrivalTime = arrivalTime;
-      this.burstTime = burstTime;
+      this.arrivalTime = arrivalTime; // tempo de chegada do processo na ready queue
+      this.burstTime = burstTime; // tempo de execução estimado do processo
     }
   }
   
-  class Scheduler {
+  class Scheduler { // escalanador de processos
     constructor() {
-      this.queue = [];
-      this.currentTime = 0;
+      this.queue = []; // array queue onde os processos são armazenados
+      this.currentTime = 0; // rastreia o tempo atual durante a execução do escalanador
     }
   
-    addProcess(process) {
+    addProcess(process) { // método que adiciona um processo a fila de processos do escalanador 
       this.queue.push(process);
     }
   
     runFCFS() {
-      this.queue.sort((a, b) => a.arrivalTime - b.arrivalTime);
+      this.queue.sort((a, b) => a.arrivalTime - b.arrivalTime); // ordena os processos por ordem de chegada -> são processados na hora que chegaram
   
-      for (const process of this.queue) {
-        if (this.currentTime < process.arrivalTime) {
-          this.currentTime = process.arrivalTime;
+      for (const process of this.queue) { // itera pela fila de processos ordenada
+        if (this.currentTime < process.arrivalTime) { // verifica se o tempo atual (currentTime) é menor que o tempo de chegada do processo atual
+          this.currentTime = process.arrivalTime; // se for menor, atualiza o tempo atual para o tempo de chegada do processo atual
         }
   
-        console.log(`Running process ${process.id} at time ${this.currentTime}`);
-        this.currentTime += process.burstTime;
+        console.log(`Running process ${process.id} at time ${this.currentTime}`);   // registra a execução do processo atual no console o ID do processo e o tempo atual
+        this.currentTime += process.burstTime; // atualiza o tempo atual adicionando o tempo de execução
       }
     }
   }
+
   // main.js
+
+  // 3 objetos da classe process
   const process1 = new Process(1, 10, 5);
   const process2 = new Process(2, 2, 3);
   const process3 = new Process(3, 4, 2);
-  
+
+  // objeto da class scheduler
   const scheduler = new Scheduler();
   
+  // adicionando cada processo a fila do escalanador com o metodo addProcess
   scheduler.addProcess(process1);
   scheduler.addProcess(process2);
   scheduler.addProcess(process3);
   
+  // executa o código FCFS
   scheduler.runFCFS();
-  
